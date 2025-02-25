d80b989df4bac72c05fb60c47e27459653f8f029
WereHound <89559870+WereHound@users.noreply.github.com>
2025-02-24 19:40:03 -0800

    2

--- index.php
+++ index.php
@@ -11,56 +11,54 @@
 
 <body>
 
-    
-        <form method="post" action="">
 
-            <label for="NExercicio" class="">Numero do Exercicio: </label>
-            <select class="form-select" name="NExercicio" id="NExercicio">
-                <option value="0">Selecione</option>
-                <option value="1">1</option>
-                <option value="2">2</option>
-                <option value="3">3</option>
-                <option value="4">4</option>
-                <option value="5">5</option>
-                <option value="6">6</option>
-                <option value="7">7</option>
-                <option value="8">8</option>
-                <option value="9">9</option>
-                <option value="10">10</option>
-                <option value="11">11</option>
-                <option value="12">12</option>
-                <option value="13">13</option>
-                <option value="14">14</option>
-                <option value="15">15</option>
-                <option value="16">16</option>
-                <option value="17">17</option>
-                <option value="18">18</option>
-                <option value="19">19</option>
-                <option value="20">20</option>
-            </select>
-            <button type="submit" class="btn btn-primary">Enviar</button>
+    <form method="post" action="">
 
-        </form>
+        <label for="NExercicio" class="">Numero do Exercicio: </label>
+        <select class="form-select" name="NExercicio" id="NExercicio">
+            <option value="0">Selecione</option>
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+            <option value="4">4</option>
+            <option value="5">5</option>
+            <option value="6">6</option>
+            <option value="7">7</option>
+            <option value="8">8</option>
+            <option value="9">9</option>
+            <option value="10">10</option>
+            <option value="11">11</option>
+            <option value="12">12</option>
+            <option value="13">13</option>
+            <option value="14">14</option>
+            <option value="15">15</option>
+            <option value="16">16</option>
+            <option value="17">17</option>
+            <option value="18">18</option>
+            <option value="19">19</option>
+            <option value="20">20</option>
+        </select>
+        <button type="submit" class="btn btn-primary">Enviar</button>
+
+    </form>
+
+    <?php
+    if (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on')
+        $link = "https";
+    else
+        $link = "http";
+
+    $link .= "://";
+
+    $link .= $_SERVER['HTTP_HOST'];
+
+    $link .= $_SERVER['REQUEST_URI']; ?>
 
 
     <?php if (($_SERVER['REQUEST_METHOD'] == 'POST') and ($_POST['NExercicio'] = '1')): ?>
 
-        <form method="post" action="Ans.php">
-
-            <div class="mb-3">
-                <label for="V1-1" class="form-label">Valor 1</label>
-                <input type="number" id="V1-1" name="V1-1" class="form-control" required="">
-            </div>
-
-            <div class="mb-3">
-                <label for="V2-1" class="form-label">Valor 2</label>
-                <input type="number" id="V2-1" name="V2-1" class="form-control" required="">
-            </div>
-
-            <input type="hidden" name="NExercicio" value="1">
-            
-            <button type="submit" class="btn btn-primary">Enviar</button>
-
+        <form action="<?php echo $link; ?>ex<?php echo $_POST['NExercicio']; ?>.php">
+            <input type="submit" value="Ir Para Destino" />
         </form>
 
     <?php elseif (($_SERVER['REQUEST_METHOD'] == 'POST') and ($_POST['NExercicio'] = '2')): ?>
