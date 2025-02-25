caf76609abd7a137b40f2de9004d4b87dc45b83c
WereHound <89559870+WereHound@users.noreply.github.com>
2025-02-25 09:06:10 -0800

    8

---
+++ Lista-3/index.php
@@ -0,0 +1,64 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title></title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+
+<body>
+
+
+    <form method="post" action="">
+
+        <label for="NExercicio" class="">Numero do Exercicio: </label>
+        <select class="form-select" name="NExercicio" id="NExercicio">
+            <option value="0">Selecione</option>
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+            <option value="4">4</option>
+            <option value="5">5</option>
+            <option value="6">6</option>
+            <option value="7">7</option>
+            <option value="8">8</option>
+            <option value="9">9</option>
+            <option value="10">10</option>
+        </select>
+        <button type="submit" class="btn btn-primary">Enviar</button>
+
+    </form>
+
+    <?php
+    if (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on')
+        $link = "https";
+    else
+        $link = "http";
+
+    $link .= "://";
+
+    $link .= $_SERVER['HTTP_HOST'];
+
+    $link .= $_SERVER['REQUEST_URI'];
+
+    $link .= ""
+        ?>
+
+
+    <?php if ($_SERVER['REQUEST_METHOD'] == 'POST'): ?>
+
+        <form action="<?php echo $link; ?>ex<?php echo $_POST['NExercicio']; ?>.php">
+            <input type="submit" value="Ir Para Destino" />
+        </form>
+
+    <?php endif; ?>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
+        crossorigin="anonymous"></script>
+</body>
+
+</html>
