4569228b55ee82d96d8da5682029f92f2558a775
WereHound <89559870+WereHound@users.noreply.github.com>
2025-02-25 11:23:59 -0800

    14

--- Lista-4/ex7.php
+++ Lista-4/ex7.php
@@ -15,33 +15,13 @@
     <form method="post" action="">
 
         <div class="mb-3">
-            <label for="V1" class="form-label">Valor 1</label>
-            <input type="decimal" id="V1" name="V1" class="form-control" required="">
+            <label for="V1" class="form-label">Data 1</label>
+            <input type="date" id="V1" name="V1" class="form-control" required="">
         </div>
 
         <div class="mb-3">
-            <label for="V2" class="form-label">Valor 2</label>
-            <input type="decimal" id="V2" name="V2" class="form-control" required="">
-        </div>
-        <div class="mb-3">
-            <label for="V3" class="form-label">Valor 3</label>
-            <input type="decimal" id="V3" name="V3" class="form-control" required="">
-        </div>
-        <div class="mb-3">
-            <label for="V4" class="form-label">Valor 4</label>
-            <input type="decimal" id="V4" name="V4" class="form-control" required="">
-        </div>
-        <div class="mb-3">
-            <label for="V5" class="form-label">Valor 5</label>
-            <input type="decimal" id="V5" name="V5" class="form-control" required="">
-        </div>
-        <div class="mb-3">
-            <label for="V6" class="form-label">Valor 6</label>
-            <input type="decimal" id="V6" name="V6" class="form-control" required="">
-        </div>
-        <div class="mb-3">
-            <label for="V7" class="form-label">Valor 7</label>
-            <input type="decimal" id="V7" name="V7" class="form-control" required="">
+            <label for="V2" class="form-label">Data 2</label>
+            <input type="date" id="V2" name="V2" class="form-control" required="">
         </div>
 
         <button type="submit" class="btn btn-primary">Enviar</button>
@@ -59,32 +39,13 @@ a diferença de dias entre as duas datas.
         try {
 
             $v1 = $_POST['V1'];
-            $v1p = "V1";
+            $date1 = date_create($v1);
             $v2 = $_POST['V2'];
-            $v2p = "V2";
-            $v3 = $_POST['V3'];
-            $v3p = "V3";
-            $v4 = $_POST['V4'];
-            $v4p = "V4";
-            $v5 = $_POST['V5'];
-            $v5p = "V5";
-            $v6 = $_POST['V6'];
-            $v6p = "V6";
-            $v7 = $_POST['V7'];
-            $v7p = "V7";
+            $date2 = date_create($v2);
 
-            $exitCon = 0;
-            $menorV = $v1;
-            while ($exitCon <= 7) {
-                $exitCon += 1;
-                if ($menorV > ${"v" . $exitCon}) {
-                    $menorV = ${"v" . $exitCon};
-                    $menorP = "V" . $exitCon;
-                }
-            }
+            $diff = date_diff($date1, $date2);
 
-
-            echo "Valor = $menorV\tPosição = $menorP";
+            echo $diff->format('Diferença de %a dias');
         } catch (Exception $e) {
             echo $e->getMessage();
         }
