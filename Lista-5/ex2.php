dd481c03279ea819d1ea768ba465a3bc386c1312
WereHound <89559870+WereHound@users.noreply.github.com>
2025-02-25 12:47:03 -0800

    16

--- Lista-5/ex2.php
+++ Lista-5/ex2.php
@@ -12,16 +12,105 @@
 <body>
 
 
-    <form method="post" action="">
+    <form method="post" action="" class="m-5">
 
-        <div class="mb-3">
-            <label for="V1" class="form-label">Palavra</label>
-            <input type="text" id="V1" name="V1" class="form-control" required="">
+        <div class="row">
+            <div class="col">
+                <label for="Nome-1">Nome</label>
+                <input type="text" id="Nome-1" name="Nome-1" class="form-control" placeholder="Mark">
+            </div>
+            <div class="col">
+                <label for="Nota1-1">Nota 1</label>
+                <input type="decimal" id="Nota1-1" name="Nota1-1" class="form-control" placeholder="0-10">
+            </div>
+            <div class="col">
+                <label for="Nota2-1">Nota 2</label>
+                <input type="decimal" id="Nota2-1" name="Nota2-1" class="form-control" placeholder="0-10">
+            </div>
+            <div class="col">
+                <label for="Nota3-1">Nota 3</label>
+                <input type="decimal" id="Nota3-1" name="Nota3-1" class="form-control" placeholder="0-10">
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col">
+                <label for="Nome-2">Nome</label>
+                <input type="text" id="Nome-2" name="Nome-2" class="form-control" placeholder="Mark">
+            </div>
+            <div class="col">
+                <label for="Nota1-2">Nota 1</label>
+                <input type="decimal" id="Nota1-2" name="Nota1-2" class="form-control" placeholder="0-10">
+            </div>
+            <div class="col">
+                <label for="Nota2-2">Nota 2</label>
+                <input type="decimal" id="Nota2-2" name="Nota2-2" class="form-control" placeholder="0-10">
+            </div>
+            <div class="col">
+                <label for="Nota3-2">Nota 3</label>
+                <input type="decimal" id="Nota3-2" name="Nota3-2" class="form-control" placeholder="0-10">
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col">
+                <label for="Nome-3">Nome</label>
+                <input type="text" id="Nome-3" name="Nome-3" class="form-control" placeholder="Mark">
+            </div>
+            <div class="col">
+                <label for="Nota1-3">Nota 1</label>
+                <input type="decimal" id="Nota1-3" name="Nota1-3" class="form-control" placeholder="0-10">
+            </div>
+            <div class="col">
+                <label for="Nota2-3">Nota 2</label>
+                <input type="decimal" id="Nota2-3" name="Nota2-3" class="form-control" placeholder="0-10">
+            </div>
+            <div class="col">
+                <label for="Nota3-3">Nota 3</label>
+                <input type="decimal" id="Nota3-3" name="Nota3-3" class="form-control" placeholder="0-10">
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col">
+                <label for="Nome-4">Nome</label>
+                <input type="text" id="Nome-4" name="Nome-4" class="form-control" placeholder="Mark">
+            </div>
+            <div class="col">
+                <label for="Nota1-4">Nota 1</label>
+                <input type="decimal" id="Nota1-4" name="Nota1-4" class="form-control" placeholder="0-10">
+            </div>
+            <div class="col">
+                <label for="Nota2-4">Nota 2</label>
+                <input type="decimal" id="Nota2-4" name="Nota2-4" class="form-control" placeholder="0-10">
+            </div>
+            <div class="col">
+                <label for="Nota3-4">Nota 3</label>
+                <input type="decimal" id="Nota3-4" name="Nota3-4" class="form-control" placeholder="0-10">
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col">
+                <label for="Nome-5">Nome</label>
+                <input type="text" id="Nome-5" name="Nome-5" class="form-control" placeholder="Mark">
+            </div>
+            <div class="col">
+                <label for="Nota1-5">Nota 1</label>
+                <input type="decimal" id="Nota1-5" name="Nota1-5" class="form-control" placeholder="0-10">
+            </div>
+            <div class="col">
+                <label for="Nota2-5">Nota 2</label>
+                <input type="decimal" id="Nota2-5" name="Nota2-5" class="form-control" placeholder="0-10">
+            </div>
+            <div class="col">
+                <label for="Nota3-5">Nota 3</label>
+                <input type="decimal" id="Nota3-5" name="Nota3-5" class="form-control" placeholder="0-10">
+            </div>
         </div>
 
         <button type="submit" class="btn btn-primary">Enviar</button>
     </form>
-
 
 
     <?php
@@ -35,15 +124,27 @@ média das notas (do maior para o menor).
 
         try {
 
-            $v1 = $_POST['V1'];
 
-            $count = 0;
-            foreach ($v1 as $i) {
-                $count++;
+            $array = [];
+            for ($i = 1; $i <= 5; $i++) {
+
+                ${"item" . $i} = ["Chave" => $_POST['Nome-' . $i], "Valor" => (($_POST['Nota1-' . $i] + $_POST['Nota2-' . $i] + $_POST['Nota3-' . $i]) / 3)];
+                $array[] = ${"item" . $i};
             }
 
 
-            echo "Número de caracteres = " . $count;
+            function sorter($a, $b)
+            {
+                return strcmp($b["Valor"], $a["Valor"]);
+            }
+
+            usort($array, "sorter");
+
+
+            foreach ($array as $value) {
+                echo "Chave = " . $value["Chave"] . "\tValor = " . $value["Valor"] . "</br>";
+            }
+
         } catch (Exception $e) {
             echo $e->getMessage();
         }
